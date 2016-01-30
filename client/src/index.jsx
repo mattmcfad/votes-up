c5f5dc813bfc127061baa69f9c3c0b4b3ca1cb24
Matt McFadyen <mmcfadyen91@gmail.com>
2016-01-30 05:10:05 -0500

    react router :fire:

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Router, { Route } from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
-ReactDOM.render(
-  <Voting pair={pair} />,
-  document.getElementById('app')
-);
+const routes = <Route component={App}>
+  <Route path="/results" component={Results} />
+  <Route path="/" component={Voting} />
+</Route>;
+
+ReactDOM.render(<Router>{routes}</Router>, document.getElementById('app'));
